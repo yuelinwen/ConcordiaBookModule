995d1ae30c9a0297280735504ccd73967f933a09
Yuelin Wen <wenyuelinca@gmail.com>
2025-10-16 13:08:30 -0400

    search filter feature done

--- myproject/textbooks/urls.py
+++ myproject/textbooks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TextbookListView.as_view(), name='textbook_list'),
-    path('<str:course_code>/', views.TextbookListView.as_view(), name='textbook_by_course')
+    path('<str:course_code>/', views.TextbookListView.as_view(), name='textbook_by_course'),
+    path('add/', views.TextbookListView.as_view(), name='add_book'),
 ]
--- myproject/textbooks/views/textbooks_views.py
+++ myproject/textbooks/views/textbooks_views.py
@@ -12,15 +12,22 @@ class TextbookListView(ListView):
         return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        q = self.request.GET.get('q', '').strip()
-        if q:
-            if ' ' not in q and len(q) <= 20:
-                return Book.objects.filter(course_code__iexact=q)
-            return Book.objects.filter(title__icontains=q)
-        course_code = self.kwargs.get('course_code')
-        if course_code:
-            return Book.objects.filter(course_code__iexact=course_code)
-        return Book.objects.all()
+        course_code_q = self.request.GET.get('courseCode', '').strip()
+        availability = self.request.GET.get('availability', '').lower() == 'true'
+
+        queryset = Book.objects.all()
+        
+        # read course code from url if provided
+        path_course_code = self.kwargs.get('course_code')
+        if path_course_code:
+            queryset = queryset.filter(course_code__iexact=path_course_code)
+            
+        # if query parameter courseCode is provided, filter again
+        if course_code_q:
+            queryset = queryset.filter(course_code__iexact=course_code_q)
+        if availability:
+            queryset = queryset.filter(availability_status=True)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
